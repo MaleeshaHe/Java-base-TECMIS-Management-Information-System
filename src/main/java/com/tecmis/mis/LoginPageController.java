83d4e3c8051f19f218cda24e953e85e0f03b0ef1
Nimesh Rangana <105283769+nim3xh@users.noreply.github.com>
2023-03-14 22:05:26 +0530

    Add files via upload
    added lecturer logging

--- src/main/java/com/tecmis/mis/LoginPageController.java
+++ src/main/java/com/tecmis/mis/LoginPageController.java
@@ -58,6 +58,7 @@ public class LoginPageController implements Initializable {
     String passwordl;
 
     int userId;
+    int depId;
     String userTg;
 
     @Override
@@ -93,11 +94,12 @@ public class LoginPageController implements Initializable {
 
                             userId = Integer.parseInt(rs.getString(1));
                             userTg = rs.getString("tgnum");
+                            depId = Integer.parseInt(rs.getString("depId"));
 
                             Set<String> privileges = new HashSet<>();
                             privileges.add("adminPrivilege");
 
-                            UserSession.getInstance(String.valueOf(userId), userTg, privileges);
+                            UserSession.getInstance(String.valueOf(userId), userTg, privileges,String.valueOf(depId));
 
                             FXMLLoader loader = new FXMLLoader(getClass().getResource("admin/admin-home.fxml"));
                             root = loader.load();
@@ -113,11 +115,12 @@ public class LoginPageController implements Initializable {
 
                             userId = Integer.parseInt(rs.getString(1));
                             userTg = rs.getString("tgnum");
+                            depId = Integer.parseInt(rs.getString("depId"));
 
                             Set<String> privileges = new HashSet<>();
                             privileges.add("studentPrivilege");
 
-                            UserSession.getInstance(String.valueOf(userId), userTg, privileges);
+                            UserSession.getInstance(String.valueOf(userId), userTg, privileges,String.valueOf(depId));
 
                             FXMLLoader loader = new FXMLLoader(getClass().getResource("student/student-home.fxml"));
                             root = loader.load();
@@ -133,13 +136,35 @@ public class LoginPageController implements Initializable {
 
                             userId = Integer.parseInt(rs.getString(1));
                             userTg = rs.getString("tgnum");
+                            depId = Integer.parseInt(rs.getString("depId"));
 
                             Set<String> privileges = new HashSet<>();
-                            privileges.add("technicalOfficerPrivilege");
+                            privileges.add("studentPrivilege");
 
-                            UserSession.getInstance(String.valueOf(userId), userTg, privileges);
+                            UserSession.getInstance(String.valueOf(userId), userTg, privileges,String.valueOf(depId));
 
                             FXMLLoader loader = new FXMLLoader(getClass().getResource("technical_officer/technical-officer-home.fxml"));
+                            root = loader.load();
+                            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                            scene = new Scene(root, 1050,600);
+                            stage.setScene(scene);
+                            stage.centerOnScreen();
+                            stage.show();
+                            stage.resizableProperty().setValue(false);
+                            new FadeIn(root).play();
+
+                        }else if (rs.getString(12).equals("Lecturer")) {
+
+                            userId = Integer.parseInt(rs.getString(1));
+                            userTg = rs.getString("tgnum");
+                            depId = Integer.parseInt(rs.getString("depId"));
+
+                            Set<String> privileges = new HashSet<>();
+                            privileges.add("lecturerPrivilege");
+
+                            UserSession.getInstance(String.valueOf(userId), userTg, privileges,String.valueOf(depId));
+
+                            FXMLLoader loader = new FXMLLoader(getClass().getResource("lecturer/lecturer-home.fxml"));
                             root = loader.load();
                             stage = (Stage)((Node)event.getSource()).getScene().getWindow();
                             scene = new Scene(root, 1050,600);
