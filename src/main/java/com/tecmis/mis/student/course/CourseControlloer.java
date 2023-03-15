d90074cdb0e7578a78c74f3e05e26f00293ee7f1
Maleesha Herath <malishaherath77@gmail.com>
2023-03-15 15:04:43 +0530

    add course enroll system

--- src/main/java/com/tecmis/mis/student/course/CourseControlloer.java
+++ src/main/java/com/tecmis/mis/student/course/CourseControlloer.java
@@ -1,6 +1,8 @@
 package com.tecmis.mis.student.course;
 
+import animatefx.animation.FadeIn;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
 import com.tecmis.mis.UserSession;
 import com.tecmis.mis.admin.course.EditCourseControlloer;
 import com.tecmis.mis.admin.notice.NoticeController;
@@ -12,30 +14,38 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 
 import java.awt.*;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class CourseControlloer implements Initializable {
+
+    @FXML
+    private JFXComboBox<String> comboCourse;
     @FXML
     private TableColumn<CourseDetails, String> materialCol;
 
@@ -72,6 +82,25 @@ public class CourseControlloer implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         loadData();
+        comboboxDataLoad();
+    }
+
+    private void comboboxDataLoad(){
+        try {
+            connection = DbConnect.getConnect();
+            query = "SELECT * FROM course";
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+
+            ObservableList data = FXCollections.observableArrayList();
+
+            while (resultSet.next()){
+                data.add(new String(resultSet.getString("courseName")));
+            }
+            comboCourse.setItems(data);
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 
     @FXML
@@ -121,6 +150,10 @@ public class CourseControlloer implements Initializable {
                     } else {
 
                         Hyperlink openMaterial = new Hyperlink("Open");
+                        Image image = new Image(getClass().getResourceAsStream("..\\..\\..\\..\\..\\images\\delete-24.png"));
+                        JFXButton deleteBtn = new JFXButton("");
+                        deleteBtn.setGraphic(new ImageView(image));
+
                         openMaterial.setStyle(
                                 " -fx-cursor: hand ;"
                                         + "-fx-border-radius:5;"
@@ -128,6 +161,10 @@ public class CourseControlloer implements Initializable {
                                         +"-fx-text-fill:#000000;"
                                         + "-fx-border-color:#cecece;"
                         );
+
+                        deleteBtn.setOnMouseClicked((MouseEvent event) -> {
+                            deleteC();
+                        });
 
                         openMaterial.setOnMouseClicked((MouseEvent event) -> {
 
@@ -148,7 +185,7 @@ public class CourseControlloer implements Initializable {
                             }
                         });
 
-                        HBox managebtn = new HBox(openMaterial);
+                        HBox managebtn = new HBox(openMaterial,deleteBtn);
                         managebtn.setStyle("-fx-alignment:center");
                         HBox.setMargin(openMaterial, new Insets(2, 2, 3, 3));
                         setGraphic(managebtn);
@@ -161,5 +198,78 @@ public class CourseControlloer implements Initializable {
         };
         materialCol.setCellFactory(cellFoctory);
         courseTable.setItems(courseList);
+    }
+
+    @FXML
+    private void enrollCourse(){
+        String coursename = comboCourse.getValue();
+        String coursecode = null;
+        try {
+            connection = DbConnect.getConnect();
+            query = "SELECT courseCode FROM course WHERE courseName='"+coursename+"'";
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                coursecode = resultSet.getString("courseCode");
+            }
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+        try {
+            connection = DbConnect.getConnect();
+            query = "INSERT INTO studentcourse (tgnum,courseCode) VALUES (?,?)";
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1,UserSession.getUserTgNum());
+            preparedStatement.setString(2,coursecode);
+            preparedStatement.executeUpdate();
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("successfull");
+            alert.setContentText("successfully Enroll course");
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if(result.get() == ButtonType.OK){
+                loadData();
+            }
+
+        }catch (Exception e){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setContentText("Already added this course");
+            alert.showAndWait();
+        }
+    }
+
+    public void deleteC(){
+        if(courseTable.getSelectionModel().getSelectedItem() != null){
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Delete");
+            alert.setContentText("Are you sure delete this one");
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if(result.get() == ButtonType.OK) {
+                try {
+                    courseDetails = courseTable.getSelectionModel().getSelectedItem();
+                    query = "DELETE FROM `studentcourse` WHERE courseCode='" + courseDetails.getCourseCode() + "'";
+                    connection = DbConnect.getConnect();
+                    preparedStatement = connection.prepareStatement(query);
+                    preparedStatement.execute();
+                    refreshTable();
+
+                } catch (SQLException ex) {
+                    Logger.getLogger(NoticeController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setContentText("If you want to delete, First you select the row that you want to delete");
+            alert.showAndWait();
+        }
     }
 }
