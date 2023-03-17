718ed4957d148547b748f97803efbcc4a00c6d62
Nimesh Rangana <105283769+nim3xh@users.noreply.github.com>
2023-03-17 21:05:47 +0530

    lecturer-student-medical-view-added

---
+++ src/main/java/com/tecmis/mis/lecturer/student/medical/MedicalDetails.java
@@ -0,0 +1,62 @@
+package com.tecmis.mis.lecturer.student.medical;
+
+public class MedicalDetails {
+    private int m_id;
+    private String tgnum;
+    private String fname;
+    private String m_title;
+    private String startDate;
+    private String endDate;
+    private byte[] documents;
+    public MedicalDetails(int m_id,String tgnum, String fname, String m_title, String startDate, String endDate, byte[] documents) {
+        this.m_id = m_id;
+        this.tgnum=tgnum;
+        this.fname=fname;
+        this.m_title = m_title;
+        this.startDate=startDate;
+        this.endDate=endDate;
+        this.documents=documents;
+    }
+
+    public int getM_id() {
+        return m_id;
+    }
+
+    public void setM_id(int m_id) {
+        this.m_id = m_id;
+    }
+
+    public String getFname(){
+        return fname;
+    }
+    public String getTgnum(){
+        return tgnum;
+    }
+    public String getM_title() {
+        return m_title;
+    }
+    public String getStartDate(){
+        return startDate;
+    }
+    public String getEndDate(){
+        return endDate;
+    }
+    public void setTgnum(String tgnum){
+        this.tgnum=tgnum;
+    }
+    public void setFname(String fname){
+        this.tgnum=tgnum;
+    }
+    public void setM_title(String m_title) {
+        this.m_title = m_title;
+    }
+    public void setStartDate(String startDate){
+        this.startDate=startDate;
+    }
+    public void setEndDate(String endDate){
+        this.endDate=endDate;
+    }
+    public void setDocuments(byte[] documents){
+        this.documents=documents;
+    }
+}
