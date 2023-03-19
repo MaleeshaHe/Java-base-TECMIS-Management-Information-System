c2c6afa29248d5f0e13caedfbf71d3ec51842c46
Maleesha Herath <malishaherath77@gmail.com>
2023-03-19 14:16:12 +0530

    changed page size

--- src/main/java/com/tecmis/mis/admin/user/AddUserControlloer.java
+++ src/main/java/com/tecmis/mis/admin/user/AddUserControlloer.java
@@ -194,7 +194,7 @@ public class AddUserControlloer implements Initializable {
                 if(result.get() == ButtonType.OK){
                     Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../admin-home.fxml")));
                     stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-                    scene = new Scene(root, 1050,600);
+                    scene = new Scene(root, 1080,610);
                     stage.setScene(scene);
                     stage.centerOnScreen();
                     stage.show();
